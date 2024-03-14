ee287213413abb0508b6874655aa7d25bdd7075d
marysanders <lyremary@yahoo.com>
2024-03-14 17:51:39 -0500

    Rework divs and flex items to move button to top

--- js-file.js
+++ js-file.js
@@ -1,8 +1,25 @@
-let container = document.querySelector(".container");
+let outerContainer = document.querySelector(".container");
+
+let buttonContainer = document.createElement("div");
+buttonContainer.setAttribute("class", "btn-div");
+outerContainer.appendChild(buttonContainer);
+
+let promptButton = document.createElement("button");
+promptButton.textContent = "New Grid";
+promptButton.setAttribute("class", "button");
+buttonContainer.appendChild(promptButton);
+
+let gridContainer = document.createElement("div");
+gridContainer.setAttribute("class", "gridContainer");
+outerContainer.appendChild(gridContainer);
+
+let grid = document.createElement("div");
+grid.setAttribute("class", "grid");
+gridContainer.appendChild(grid);
 
 for (let i = 0; i < 16; i++) {
   let col = document.createElement("div");
-  container.appendChild(col);
+  grid.appendChild(col);
 
   for (let j = 0; j < 16; j++) {
     const row = document.createElement("div");
